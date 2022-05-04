df08846b726167918a5cd3fef9e559049763184b
HENINTSAMBATRA\monpc <najoromevasjaspers@gmail.com>
2022-05-04 10:54:29 +0300

    color inverse
    inverse Collor

--- assets/js/spiderman.js
+++ assets/js/spiderman.js
@@ -1,1 +1,13 @@
+//Animation General
+
+//Effet hover sur le botton
 let btniversrse = document.getElementsByClassName('btninverse');
+console.log(btniversrse);
+
+for (let i = 0; i < btniversrse.length; i++) {
+    btniversrse[i].addEventListener('mouseover', () => {
+        btniversrse[i].style.backgroundColor = '#b11313';
+        btniversrse[i].style.color = 'white';
+    })
+
+}
