566423ee71d5e27a4d453a9c44909f129036c913
HENINTSAMBATRA\monpc <najoromevasjaspers@gmail.com>
2022-05-04 16:43:51 +0300

    footer-icone
    hover sur js

--- assets/js/spiderman.js
+++ assets/js/spiderman.js
@@ -8,6 +8,24 @@ for (let i = 0; i < btniversrse.length; i++) {
     btniversrse[i].addEventListener('mouseover', () => {
         btniversrse[i].style.backgroundColor = '#b11313';
         btniversrse[i].style.color = 'white';
-    })
+        btniversrse[i].style.fontWeight = 'bold';
+        btniversrse[i].style.boxShadow = '5px 5px 30px white';
+    });
+    btniversrse[i].addEventListener('mouseover', () => {
+        btniversrse[i].style.backgroundColor = 'white';
+        btniversrse[i].style.color = '#b11313';
+        btniversrse[i].style.boxShadow = 'none';
+    });
 
+}
+//Effet sur le footer icones
+
+let footericone = document.getElementsByTagName('footer-icones');
+for (let i = 0; i < footericone.length; i++) {
+    btniversrse[i].addEventListener('mouseover', () => {
+        footericone[i].style.color = 'white';
+    });
+    btniversrse[i].addEventListener('mouseover', () => {
+        footericone[i].style.color = 'black';
+    });
 }
