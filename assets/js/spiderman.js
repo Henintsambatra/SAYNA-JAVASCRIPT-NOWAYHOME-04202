676d465bc60e2780ea03292a7d107674301288e4
HENINTSAMBATRA\monpc <najoromevasjaspers@gmail.com>
2022-05-05 15:21:32 +0300

    jquery
    animation

--- assets/js/spiderman.js
+++ assets/js/spiderman.js
@@ -11,21 +11,41 @@ for (let i = 0; i < btniversrse.length; i++) {
         btniversrse[i].style.fontWeight = 'bold';
         btniversrse[i].style.boxShadow = '5px 5px 30px white';
     });
-    btniversrse[i].addEventListener('mouseover', () => {
+    btniversrse[i].addEventListener('mouseout', () => {
         btniversrse[i].style.backgroundColor = 'white';
         btniversrse[i].style.color = '#b11313';
         btniversrse[i].style.boxShadow = 'none';
     });
 
 }
-//Effet sur le footer icones
 
-let footericone = document.getElementsByTagName('footer-icones');
-for (let i = 0; i < footericone.length; i++) {
-    btniversrse[i].addEventListener('mouseover', () => {
-        footericone[i].style.color = 'white';
+//Effet sur le footer icones
+let footericones = document.getElementsByClassName('footer-icones');
+for (let i = 0; i < footericones.length; i++) {
+    footericones[i].addEventListener('mouseover', () => {
+        footericones[i].style.color = 'white';
     });
-    btniversrse[i].addEventListener('mouseover', () => {
-        footericone[i].style.color = 'black';
+    footericones[i].addEventListener('mouseout', () => {
+        footericones[i].style.color = 'black';
     });
 }
+
+//les rubriques du menu doivent être surligné
+let menu = document.querySelectorAll('li');
+
+menu.forEach(function(li) {
+    li.addEventListener('mouseover', () => {
+        li.style.textDecoration = 'line-through';
+    });
+    li.addEventListener('mouseout', () => {
+        li.style.textDecoration = 'none';
+    });
+});
+
+//apparaître avec un fade in en démarrant de la gauche(mouvement de gauche à droite).
+$(document).ready(function() { //$ =jquery
+    $(".box").animate({
+        left: '30px ',
+    });
+    $(".description").fadeIn(5000);
+});
